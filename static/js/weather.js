2e6f3488989a0cbbc28b7d973f4d2c5ff8525382
codename-art <120071994>
2017-12-22 01:38:08 +0300

    add workaround for int overflow in s2_cell_id

--- static/js/weather.js
+++ static/js/weather.js
@@ -178,7 +178,7 @@ function createCellAlert(item) {
         cell.fillColor = '#ffff00'
     } else if (item.severity === 2) {
         cell.fillColor = '#ff0000'
-        console.log(cell.fillColor)
+        // console.log(cell.fillColor)
     }
     return cell
 }
@@ -247,15 +247,11 @@ function getMainS2Cell() {
         {'lat': bounds.getSouthWest().lat(), 'lng': bounds.getNorthEast().lng()}
     ]
     var jstsViewport = createJstsPolygon(geometryFactory, viewportPath)
-    var viewportArea = jstsViewport.getArea()
     var maxCoverageData
     $.each(mapData.weather, function (i, s2cell) {
         var jstsS2cell = createJstsPolygon(geometryFactory, s2cell.vertices)
         var area = jstsViewport.intersection(jstsS2cell).getArea()
-        if (
-            jstsS2cell.getArea() < area * 2 || // more than a half of the cell contains on the screen
-            viewportArea < area * 2 // more then a half of the screen covered by cell
-        ) {
+        if (jstsS2cell.getArea() < area * 2) { // more than a half of the cell contains on the screen
             maxCoverageData = s2cell
         }
     })
