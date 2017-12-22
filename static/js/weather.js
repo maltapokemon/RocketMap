c5753637ed1dbc76ed44ef2b196c6c3316e2fd98
codename-art <120071994>
2017-12-23 00:37:47 +0300

    add white icon set for weather icons

--- static/js/weather.js
+++ static/js/weather.js
@@ -111,9 +111,10 @@ function safeDelMarker(item) {
 /**
  * Creates path for weather icon based on gameplay_weather and world_time
  * @param item
+ * @param dark dark or light version of image, default is dark
  * @returns {*}
  */
-function getWeatherImageUrl(item) {
+function getWeatherImageUrl(item, dark = true) {
     var imageUrl
     if (item.severity === 1) {
       imageUrl = '/static/images/weather/' + weatherImages[15]
@@ -127,6 +128,9 @@ function getWeatherImageUrl(item) {
         }
     } else {
         imageUrl = '/static/images/weather/' + weatherImages[item.gameplay_weather]
+    }
+    if (!dark) {
+        imageUrl = imageUrl.replace('weather_', 'weather_light_')
     }
     return imageUrl
 }
@@ -217,7 +221,7 @@ function updateMainCellWeather() {
     }
     var s2Cell = getMainS2Cell()
     if (s2Cell != null) {
-        var imgUrl = getWeatherImageUrl(s2Cell)
+        var imgUrl = getWeatherImageUrl(s2Cell, false)
         var icon = document.createElement('img')
         icon.setAttribute('src', imgUrl)
         $weatherInfo.appendChild(icon)
