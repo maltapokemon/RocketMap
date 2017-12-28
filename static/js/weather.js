d235026fa2f1cdd3f6a8109b467c1ee7f153eb55
SkOODaT <skoodat@live.com>
2017-12-27 20:47:19 -0500

    Use Relative Paths To Weather Icons
    Use Relative Paths To Weather Icons.

--- static/js/weather.js
+++ static/js/weather.js
@@ -118,17 +118,17 @@ function safeDelMarker(item) {
 function getWeatherImageUrl(item, dark = true) {
     var imageUrl
     if (item.severity === 1) {
-      imageUrl = '/static/images/weather/' + weatherImages[15]
+      imageUrl = 'static/images/weather/' + weatherImages[15]
     } else if (item.severity === 2) {
-      imageUrl = '/static/images/weather/' + weatherImages[16]
+      imageUrl = 'static/images/weather/' + weatherImages[16]
     } else if (item.world_time === 2) { // night
         if (![1, 3].includes(item.gameplay_weather)) { // common icons for day and night
-          imageUrl = '/static/images/weather/' + weatherImages[item.gameplay_weather]
+          imageUrl = 'static/images/weather/' + weatherImages[item.gameplay_weather]
         } else { // clear and partly cloudy
-          imageUrl = '/static/images/weather/' + weatherImages[item.gameplay_weather + 10]
+          imageUrl = 'static/images/weather/' + weatherImages[item.gameplay_weather + 10]
         }
     } else {
-      imageUrl = '/static/images/weather/' + weatherImages[item.gameplay_weather]
+      imageUrl = 'static/images/weather/' + weatherImages[item.gameplay_weather]
     }
     if (!dark && item.severity == 0) {
       imageUrl = imageUrl.replace('weather_', 'weather_light_')
